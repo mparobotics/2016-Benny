4dababf8806b9a51c7742c8ecde2d578f873dc57
WilliamKluge <wkluge17@moundsparkacademy.org>
2016-02-18 15:22:49 -0600

    new

--- src/org/usfirst/frc/team3926/robot/Robot.java
+++ src/org/usfirst/frc/team3926/robot/Robot.java
@@ -1,26 +1,58 @@
 
 package org.usfirst.frc.team3926.robot;
 
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ShapeMode;
+
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
 
 
 public class Robot extends IterativeRobot {
+	
+	static final int TALON_BACK_LEFT_CAN_ID 	= 1;
+    static final int TALON_FRONT_LEFT_CAN_ID	= 2;
+    static final int TALON_BACK_RIGHT_CAN_ID 	= 3;
+    static final int TALON_FRONT_RIGHT_CAN_ID 	= 4;
+    static final int TALON_ROLLER_CAN_ID 		= 5;
+    static final int TALON_ROLLER_ARM_CAN_ID	= 6;
+    static final int TALON_WEDGE_ARM_CAN_ID 	= 7;
+
+    static final int ROLLER_ARM_EXTENDED_DIGITAL_INPUT 	= 0;
+    static final int ROLLER_ARM_RETRACTED_DIGITAL_INPUT	= 1;
+    static final int WEDGE_ARM_EXTENDED_DIGITAL_INPUT 	= 2;
+    static final int WEDGE_ARM_RETRACTED_DIGITAL_INPUT	= 3;
+    static final int LEFT_ENCODER_A_DIGITAL_INPUT 		= 4;
+    static final int LEFT_ENCODER_B_DIGITAL_INPUT 		= 5;
+    static final int LEFT_ENCODER_INDEX_DIGITAL_INPUT	= 6;
+    static final int RIGHT_ENCODER_A_DIGITAL_INPUT 		= 7;
+    static final int RIGHT_ENCODER_B_DIGITAL_INPUT 		= 8;
+    static final int RIGHT_ENCODER_INDEX_DIGITAL_INPUT 	= 9;
+
+    static final int LEFT_JOYSTICK_ID 	= 0;
+    static final int RIGHT_JOYSTICK_ID  = 1;
+    static final int XBOX_JOYSTICK_ID   = 2;
+
+	CANTalon talonSRX_FL; //Front Left
+	CANTalon talonSRX_BL; //Back Left
 	CANTalon talonSRX_FR; //Front Right
-    CANTalon talonSRX_FL; //Front Left
     CANTalon talonSRX_BR; //Back Right
-    CANTalon talonSRX_BL; //Back Left
     RobotDrive driveSystem; //Controller class for operating TankDrive
+
     Joystick leftStick; //Joystick object for the left hand
     double leftInput;
     Joystick rightStick; //Joystick object for the right hand
     double rightInput;
     
-    Joystick XBox; //The joystick for the supplimentary driver
+    Joystick xbox; //The joystick for the supplimentary driver
 
     Encoder leftEncoder; //Encoder to help us measure our distance traveled
     Encoder rightEncoder; //Encoder on the right side
@@ -30,22 +62,75 @@ public class Robot extends IterativeRobot {
     CANTalon rollerArm; //Motor to move the roller's arm
     CANTalon roller; //Motor to spin the roller
     
-    DigitalInput wedgeArmLimitRetracted; //Limit switch to prevent the wedge's arm from trying to go to far back
-    DigitalInput wedgeArmLimitExtended; //Limit switch to prevent the wedge's arm from over-extending
+    DigitalInput wedgeArmRetracted; //Limit switch to prevent the wedge's arm from trying to go to far back
+    DigitalInput wedgeArmExtended; //Limit switch to prevent the wedge's arm from over-extending
     CANTalon wedgeArm; //Motor to control the wedge
 	
     public void robotInit() {
-        
+		talonSRX_FL = new CANTalon(TALON_FRONT_LEFT_CAN_ID);
+		talonSRX_BL = new CANTalon(TALON_BACK_LEFT_CAN_ID);
+		talonSRX_FR = new CANTalon(TALON_FRONT_RIGHT_CAN_ID);
+		talonSRX_BR = new CANTalon(TALON_BACK_RIGHT_CAN_ID);
+		driveSystem = new RobotDrive(talonSRX_FL, talonSRX_BL, talonSRX_FR, talonSRX_BR);
+
+        leftStick = new Joystick(LEFT_JOYSTICK_ID);
+        rightStick = new Joystick(RIGHT_JOYSTICK_ID);
+
+        xbox = new Joystick(XBOX_JOYSTICK_ID);
+
+        leftEncoder = new Encoder(LEFT_ENCODER_A_DIGITAL_INPUT, LEFT_ENCODER_B_DIGITAL_INPUT,
+                LEFT_ENCODER_INDEX_DIGITAL_INPUT);
+        rightEncoder = new Encoder(RIGHT_ENCODER_A_DIGITAL_INPUT, RIGHT_ENCODER_B_DIGITAL_INPUT,
+                RIGHT_ENCODER_INDEX_DIGITAL_INPUT);
+
+        rollerArmExtended = new DigitalInput(ROLLER_ARM_EXTENDED_DIGITAL_INPUT);
+        rollerArmRetracted = new DigitalInput(ROLLER_ARM_RETRACTED_DIGITAL_INPUT);
+        rollerArm = new CANTalon(TALON_ROLLER_ARM_CAN_ID);
+        roller = new CANTalon(TALON_ROLLER_CAN_ID);
+
+        wedgeArmRetracted = new DigitalInput(WEDGE_ARM_RETRACTED_DIGITAL_INPUT);
+        wedgeArmExtended = new DigitalInput(WEDGE_ARM_EXTENDED_DIGITAL_INPUT);
+        wedgeArm = new CANTalon(TALON_WEDGE_ARM_CAN_ID);
+
     }
     ////END robotInit()////
     
-    public void autonomousInit() {
-    	
+    public void autonomousPeriodic() {
+    	runCamera();
+
+
     }
-    ////END autonomousInit()////
+    ////END autonomousPeriodic()////
 
     public void teleopPeriodic() {
-        
+        runCamera();
+
+        boolean wedgeRan;
+
+        runDrive(); //Run the main drive control
+
+        if (xbox.getRawButton(1)) { //A button
+            runMotor(getState(wedgeArmRetracted, wedgeArmMin), getState(wedgeArmRetracted, wedgeArmMin), wedgeArm,
+                    xbox.getRawAxis(2));
+            wedgeRan = true;
+        } else {
+            wedgeArm.set(0);
+            wedgeRan = false;
+        }
+
+        if (xbox.getRawButton(2) && !wedgeRan) { //B button
+            runMotor(getState(rollerArmRetracted, rollerArmMin), getState(rollerArmExtended, rollerArmMax), rollerArm,
+                    xbox.getRawAxis(2));
+        } else {
+            rollerArm.set(0);
+        }
+
+        if (xbox.getRawButton(6)) {
+            roller.set(1);
+        } else {
+            roller.set(0);
+        }
+
     }
     ////END teleopPeriodic()////   
     
@@ -102,7 +187,6 @@ public class Robot extends IterativeRobot {
 
 	        return made;
 	    }
-	    ////END limitState()////
 	////Stop LimitSwitchControl////
 	    
 	////Start ArmControl////
@@ -129,43 +213,36 @@ public class Robot extends IterativeRobot {
 	    private final double ninetyDegreeTime = 10; //In milliseconds //TODO test this
 	    private final int magicAngleTime = 10; //Angle to turn to low goal in milliseconds
 	    
-	    private String convertAction(int action) {
-	    	String actionString = "";
-	    	
-	    	switch(action) {
-	    	case 1:
-	    		actionString = "straight";
-	    		break;
-	    	case 2:
-	    		actionString = "turnLeft";
-	    		break;
-	    	case 3:
-	    		actionString = "turnRight";
-	    		break;
-	    	case 4:
-	    		actionString = "done";
-	    		break;
-	    	default:
-	    		actionString = "broken";
-	    	}
-	    	
-	    	return actionString;
-	    }
-	    
-	    private final int[] lowBarActions = {
-	    		1, 2, 1, 4, //Order of actions
-	    		116, magicAngleTime, 
-	    		
+	    private final String[] lowBarActions = {
+	    		"straight", "turnLeft", "straight", "done" //Order of actions
 	    };
+
+
 	    
 	    public String lowbarAct(int stage) {
 	    	String action = "";
 	    	
-	    	
+
 	    	
 	    	return action;
 	    }
 	////Stop AutonomousController////
+
+    ////Start CameraController////
+    int session;
+    Image frame;
+    CameraServer server;
+
+    public void runCamera() {
+        NIVision.Rect rect = new NIVision.Rect(200, 250, 100, 100);
+
+        NIVision.IMAQdxGrab(session, frame, 1);
+        NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+
+        CameraServer.getInstance().setImage(frame);
+        Timer.delay(0.005);
+    }
+    ////Stop CameraController////
 }
 ////END Robot class////
 
