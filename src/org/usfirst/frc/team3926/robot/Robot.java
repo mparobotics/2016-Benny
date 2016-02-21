f4ea7602ddb09caffab1b8842367b536933d0433
WilliamKluge <wkluge17@moundsparkacademy.org>
2016-02-21 13:36:09 -0600

    End of week zero day code

--- src/org/usfirst/frc/team3926/robot/Robot.java
+++ src/org/usfirst/frc/team3926/robot/Robot.java
@@ -1,16 +1,12 @@
 
 package org.usfirst.frc.team3926.robot;
 
-import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.DrawMode;
-import com.ni.vision.NIVision.Image;
-import com.ni.vision.NIVision.ShapeMode;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends IterativeRobot {
-	
+
 	static final int TALON_BACK_LEFT_CAN_ID 	= 1;
     static final int TALON_FRONT_LEFT_CAN_ID	= 2;
     static final int TALON_BACK_RIGHT_CAN_ID 	= 3;
@@ -44,7 +40,7 @@ public class Robot extends IterativeRobot {
     double leftInput;
     Joystick rightStick; //Joystick object for the right hand
     double rightInput;
-    
+
     Joystick xbox; //The joystick for the supplimentary driver
 
     Encoder leftEncoder; //Encoder to help us measure our distance traveled
@@ -54,19 +50,20 @@ public class Robot extends IterativeRobot {
     DigitalInput rollerArmExtended; //Limit switch to stop the roller's arm from over-extending
     CANTalon rollerArm; //Motor to move the roller's arm
     CANTalon roller; //Motor to spin the roller
-    
+
     DigitalInput wedgeArmRetracted; //Limit switch to prevent the wedge's arm from trying to go to far back
     DigitalInput wedgeArmExtended; //Limit switch to prevent the wedge's arm from over-extending
     CANTalon wedgeArm; //Motor to control the wedge
-
 
     private int rollerArmMin; //The int to store our debounce count
     private int rollerArmMax;
     private int wedgeArmMin;
     private int wedgeArmMax;
 
-    private double correctionNumber = .2;
-	
+    CameraServer server;
+
+    //private double correctionNumber = .2;
+
     public void robotInit() {
 		talonSRX_FL = new CANTalon(TALON_FRONT_LEFT_CAN_ID);
 		talonSRX_BL = new CANTalon(TALON_BACK_LEFT_CAN_ID);
@@ -92,38 +89,40 @@ public class Robot extends IterativeRobot {
         wedgeArmRetracted = new DigitalInput(WEDGE_ARM_RETRACTED_DIGITAL_INPUT);
         wedgeArmExtended = new DigitalInput(WEDGE_ARM_EXTENDED_DIGITAL_INPUT);
         wedgeArm = new CANTalon(TALON_WEDGE_ARM_CAN_ID);
+
+        server = CameraServer.getInstance();
+        server.setQuality(50);
+        server.startAutomaticCapture("cam0");
+       // server.startAutomaticCapture("cam1");
     }
     ////END robotInit()////
-    
+
     public void autonomousPeriodic() {
-    	runCamera();
+    	//runCamera();
 
-
+        Timer.delay(0.005);
     }
     ////END autonomousPeriodic()////
 
     public void teleopPeriodic() {
-        runCamera();
-
-        boolean wedgeRan;
-
         runDrive(); //Run the main drive control
 
-        if (xbox.getRawAxis(1) <= -0.1) { //Left Y
+        double rollerInput = xbox.getRawAxis(1);
+        if (rollerInput <= -0.1) { //Left Y
             rollerArmMin = getState(rollerArmRetracted, rollerArmMin);
 
-            SmartDashboard.putNumber("Out of function number", rollerArmMin);
+            //SmartDashboard.putNumber("Out of function number", rollerArmMin);
 
             if (rollerArmMin >= 30) {
-                rollerArm.set(xbox.getRawAxis(1));
+                rollerArm.set(((rollerInput * rollerInput) /2) * -1);
             } else {
                 rollerArm.set(0);
             }
-        } else if (xbox.getRawAxis(1) >= .1) { //Left Y
+        } else if (rollerInput >= .1) { //Left Y
             rollerArmMax = getState(rollerArmExtended, rollerArmMax);
 
             if (rollerArmMax >= 30) {
-                rollerArm.set(xbox.getRawAxis(1));
+                rollerArm.set((rollerInput * rollerInput)/2);
             } else {
                 rollerArm.set(0);
             }
@@ -133,90 +132,82 @@ public class Robot extends IterativeRobot {
 
         if (xbox.getRawAxis(2) >= 0.1) { //Left Trigger
             roller.set(xbox.getRawAxis(2));
-        } else if (xbox.getRawAxis(3) > 0.1) { //Right Trigger
+        } else if (xbox.getRawAxis(3) > 0.1 ) { //Right Trigger
             roller.set(xbox.getRawAxis(3) * -1);
+        } else {
+        	roller.set(0);
         }
 
-        if (xbox.getRawAxis(5) <= -0.1) { //Right Y
+        double wedgeInput = xbox.getRawAxis(5);
+        if (wedgeInput <= -0.1) { //Right Y
             wedgeArmMin = getState(wedgeArmRetracted, wedgeArmMin);
 
             if (wedgeArmMin >= 30) {
-                wedgeArm.set(xbox.getRawAxis(5));
+                wedgeArm.set(((wedgeInput * wedgeInput) * -1)/3);
             } else {
                 wedgeArm.set(0);
             }
-        } else if (xbox.getRawAxis(5) >= 0.1) { //Right Y
+        } else if (wedgeInput >= 0.1) { //Right Y
             wedgeArmMax = getState(wedgeArmExtended, wedgeArmMax);
 
             if (wedgeArmMax >= 30) {
-                wedgeArm.set(xbox.getRawAxis(5));
+                wedgeArm.set((wedgeInput * wedgeInput)/3);
             } else {
                 wedgeArm.set(0);
             }
         } else {
             wedgeArm.set(0);
         }
+        //TODO move set outside and change variables
+        
+        Timer.delay(0.005);
     }
-    ////END teleopPeriodic()////   
-    
+    ////END teleopPeriodic()////
+
     ////Start DriveFunctions////
 	    public void runDrive() {
 	        leftInput = leftStick.getY();
 	        rightInput = rightStick.getY();
-	
-	        if (rightStick.getRawButton(1)) {
-	            straightMode();
-	        }
 
-            if (Math.abs(leftInput) - Math.abs(rightInput) > .2) {
+          /*  if (Math.abs(leftInput) - Math.abs(rightInput) >= correctionNumber) {
                 if (leftInput > 0) {
                     rightInput = leftInput - correctionNumber;
                 } else {
                     rightInput = leftInput + correctionNumber;
                 }
-            } else if (Math.abs(rightInput) - Math.abs(leftInput) > .2) {
+            } else if (Math.abs(rightInput) - Math.abs(leftInput) >= correctionNumber) {
                 if (rightInput > 0) {
                     leftInput = rightInput - correctionNumber;
                 } else {
                     leftInput = rightInput + correctionNumber;
                 }
-            }
+            } */
 
-            if (rightStick.getRawButton(2)) {
-                turnFunction(rightStick.getX());
+            if (rightStick.getRawButton(1)) {
+                straightMode();
             }
 
             if (leftStick.getRawButton(1)) {
                 safteyMode();
             }
-	
+
 	        driveSystem.tankDrive(leftInput, rightInput);
 	    }
 	    ////END runDrive()////
-	
+
 	    public void straightMode() {
 	        leftInput = rightInput;
 	    }
-	    ////END strightMode()////
-	
+	    ////END straightMode()////
+
 	    public void safteyMode() {
 	        leftInput /= 2;
 	        rightInput /= 2;
 	    }
 	    ////END safteyMode()////
 
-        public void turnFunction(double input) {
-            if (input > 0) {
-                rightInput = input;
-                leftInput = input - correctionNumber;
-            } else {
-                leftInput = input;
-                rightInput = input + correctionNumber;
-            }
-        }
-        ////END turnFunction()////
 	////Stop DriveFunctions////
-	    
+
 	////Start LimitSwitchControl////
 	    public int getState(DigitalInput limit, int madeCheck) {
 
@@ -226,57 +217,63 @@ public class Robot extends IterativeRobot {
 	            madeCheck = 0;
 	        }
 
-            SmartDashboard.putNumber("In function number", madeCheck);
+            //SmartDashboard.putNumber("In function number", madeCheck);
 
 	        return madeCheck;
 	    }
 	////Stop LimitSwitchControl////
-	    
+
 	////Start ArmControl////
+
 	    /**
-	     * 
+	     *
 	     * @param retractLimitState The boolean of the retracted switch for the motor
 	     * @param extendLimitState The boolean of the extended switch for the motor
 	     * @param motor The motor that we are controlling
 	     * @param speed The speed to set that motor to
 	     */
+        /*
 	    public void runMotor(boolean retractLimitState, boolean extendLimitState, CANTalon motor, double speed) {
+            double motorSpeed = 0;
+
 	        if (!retractLimitState && speed < 0) { //It must be less than zero so that it can move away from the limit
-	            motor.set(speed);
+	            motorSpeed = speed;
 	        } else if (!extendLimitState && speed > 0) {
-	            motor.set(speed);
+	            motorSpeed = speed;
 	        } else {
-	            motor.set(0);
+	            motorSpeed = 0;
 	        }
+
+            motor.set(motorSpeed);
 	    }
+    */
 	////Stop ArmControl////
-	    
-	////Start AutonomousController////
+
+/*	////Start AutonomousController////
 	    private double deltaTime = 0; //This helps measure the time for rotations
 	    private final double ninetyDegreeTime = 10; //In milliseconds //TODO test this
 	    private final int magicAngleTime = 10; //Angle to turn to low goal in milliseconds
-	    
+
 	    private final String[] lowBarActions = {
 	    		"straight", "turnLeft", "straight", "done" //Order of actions
 	    };
 
 
-	    
+
 	    public String lowbarAct(int stage) {
 	    	String action = "";
-	    	
 
-	    	
+
+
 	    	return action;
 	    }
 	////Stop AutonomousController////
-
+*/
     ////Start CameraController////
-    int session;
-    Image frame;
-    CameraServer server;
+    //int session;
+    //Image frame;
 
-    public void runCamera() {
+/*    public void runCamera() {
         NIVision.Rect rect = new NIVision.Rect(200, 250, 100, 100);
 
         NIVision.IMAQdxGrab(session, frame, 1);
@@ -286,6 +283,7 @@ public class Robot extends IterativeRobot {
         Timer.delay(0.005);
     }
     ////Stop CameraController////
+    */
 }
 ////END Robot class////
 
